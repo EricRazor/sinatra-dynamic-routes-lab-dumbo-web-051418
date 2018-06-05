aa6c911a7ab7408148aa8a066a3c0e99dc4fcb5c
Eric Razor, Jr <eric.razorjr@gmail.com>
2018-06-05 17:22:39 -0400

    finished lab

--- app.rb
+++ app.rb
@@ -1,6 +1,49 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
   # Write your code here!
+
+  get '/reversename/:name' do
+    # do the thing that's asked (reverse the name)
+    params['name'].reverse
+  end
+
+  get '/square/:number' do
+    #square the number
+    num = params[:number].to_i ** 2
+    num.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    final_string = ""
+    (params[:number].to_i).times do
+      final_string += "#{params[:phrase]}\n" #taking each number and printing it to a new line then setting equal to new var
+    end
+    final_string
+  end
+
+#takes in a string of 5 words and returns a STRING
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    operation = params[:operation]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if operation == "add"
+      answer = number1 + number2
+    elsif operation == "subtract"
+      answer = number1 - number2
+    elsif operation == "multiply"
+      answer = number1 * number2
+    elsif operation == "divide"
+      answer = number1 / number2
+    else
+      answer = "Unable to perform this operation"
+    end
+    answer.to_s
+  end
 
 end
